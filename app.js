01cec2eeef6bd66cd58812d3135169422d8f20ad
ozlem <06ozlemozturk@gmail.com>
2023-12-26 22:53:23 +0300

    express

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+/* ------------------------------------------------
+  EXPRESSJS
+-------------------------------------------------- */
+
+/*
+ * npm init -y
+ * npm i espress dotenv
+ */
+
+/* ExpressJS Start */
+
+const express = requirel("express"); // Assing expressFramework to express variable.
+const app = express(); // run application on express.
