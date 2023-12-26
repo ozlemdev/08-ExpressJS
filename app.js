ab530ea009f347b95fb1d3b8ab077cff464bf33f
ozlem <06ozlemozturk@gmail.com>
2023-12-26 23:19:47 +0300

    exprsss

--- app.js
+++ app.js
@@ -10,5 +10,18 @@
 
 /* ExpressJS Start */
 
-const express = requirel("express"); // Assing expressFramework to express variable.
+const express = require("express"); // Assing expressFramework to express variable.
 const app = express(); // run application on express.
+
+/* ENV */
+// dosya içeriğini çalıştırma
+
+require("dotenv").config();
+//console.log(process.env);
+const HOST = process.env.HOST || "http://localhost";
+const PORT = process.env.PORT || 8000;
+/*-------------------------------------------------- */
+
+/*-------------------------------------------------- */
+app.listen(PORT, () => console.log("Running on ${HOST}:${PORT} "));
+/*-------------------------------------------------- */
