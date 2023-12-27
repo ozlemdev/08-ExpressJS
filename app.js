96384601deb95a50c75b6e55474ff0277fd7ec32
ozlem <06ozlemozturk@gmail.com>
2023-12-27 22:40:56 +0300

    route

--- app.js
+++ app.js
@@ -18,8 +18,11 @@ console.log(process.env);
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 8000;
 /*------------------------------------------------------- */
-/* HTTP_Methods & URLLS */
 
+/*------------------------------------------------------- 
+
+/* HTTP_Methods & URLLS */
+/*------------------------------------------------------- 
 app.get("/", (request, response) => {
   //response.send(" Welcome to Express");
   response.send({ message: 'called in "get" method' });
@@ -33,10 +36,28 @@ app.put("/", (request, response) =>
 app.delete("/", (request, response) =>
   response.send({ message: 'called in "delete" method' })
 );
+
 //? allow at all methods:
 app.all("/", (request, response) =>
   response.send({ message: ' "all" option allows to all method.' })
 );
+
+------------------------------------------------------- */
+/*------------------------------------------------------- 
+//? app.route('url' ) :
+
+app
+  .route("/route")
+  .get((req, res) => res.send("get"))
+  .post((req, res) => res.send("post"))
+  .put((req, res) => res.send("put"))
+  .delete((req, res) => res.send("delete"));
+
 /*------------------------------------------------------- */
+/*URL Options */
+
+/*------------------------------------------------------- */
+/*------------------------------------------------------- */
+
 //app.listen(PORT, () => console.log(`Running on http:127.0.0.1:${PORT}`));
 app.listen(PORT, () => console.log(`Running on http://${HOST}:${PORT}`));
